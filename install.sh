138a5782fe3e022b4e785bb31cfea3806ad05dc8
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-11-29 16:58:32 +0100

    feat!: Use systemd user generators (#12)

--- install.sh
+++ install.sh
@@ -5,21 +5,45 @@ set -euo pipefail
 cd "$(dirname "$0")" || exit 1
 
 DESTDIR=${DESTDIR:-/usr}
-SUDOIF=""
-if [[ $UID -ne 0 ]]; then
-    SUDOIF="sudo"
+install_locs="src/99-ublueos-udisks-automount.rules /etc/polkit-1/rules.d/99-ublueos-udisks-automount.rules
+              src/ublueos-udisks-automount-generator $DESTDIR/lib/systemd/user-generators/ublueos-udisks-automount-generator
+              src/mount_options.conf /etc/udisks2/mount_options.conf"
+
+placeIn() {
+    local src=${1:?}
+    local dest=${2:?}
+
+    if [[ ! -e $src ]]; then
+        echo >&2 "File $src does not exists"
+        return 1
+    fi
+    mkdir -vp "$(dirname "$dest")"
+    cp -vTrdb "$src" "$dest"
+}
+
+if [[ $EUID -ne 0 ]]; then
+    echo >&2 "WARNING: you might lack permissions to write to the directories. Try with 'sudo' if doesnt work."
 fi
 
-ACTION=${1:-install}
-
+ACTION="${1:-install}"
 case $ACTION in
 install)
-    $SUDOIF install -Dm755 -t "$DESTDIR"/lib/media-automount.d ./media-automount.d/*
-    $SUDOIF install -Dm755 -t "$DESTDIR"/lib/systemd/system-generators ./media-automount-generator
+    while read -r _k _v; do
+        placeIn "$_k" "$_v"
+    done <<<"$install_locs"
+    unset -v _k
     ;;
-
-uninstall)
-    $SUDOIF rm -rf "$DESTDIR"/lib/media-automount.d
-    $SUDOIF rm -f "$DESTDIR"/lib/systemd/system-generators/media-automount-generator
+uninstall | remove | rm | delete)
+    while read -r _ _v; do
+        rm -vr "$_v"
+    done <<<"$install_locs"
+    ;;
+"")
+    echo >&2 "Available actions: install, uninstall, remove, rm, delete"
+    exit 0
+    ;;
+*)
+    echo >&2 "Action no supported: $ACTION"
+    exit 1
     ;;
 esac
